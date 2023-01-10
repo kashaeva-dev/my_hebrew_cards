61deae098e49c5a024a7e745691a0e1ae717f96a
Nataliya Kashaeva <n.a.kashaeva@gmail.com>
2023-01-10 23:55:04 +0300

    Test static root

--- conf/settings.py
+++ conf/settings.py
@@ -125,7 +125,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "images"),
-]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
